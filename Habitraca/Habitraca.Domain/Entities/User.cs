0d0e3d1fcb651cc11b32756a4bfac6b42de77f18
CHUKWUDI INNOCENT <125125776+Innochu@users.noreply.github.com>
2024-03-04 20:07:39 +0100

    aspnetcore.Identity

--- Habitraca/Habitraca.Domain/Entities/User.cs
+++ Habitraca/Habitraca.Domain/Entities/User.cs
@@ -1,6 +1,19 @@
-﻿namespace Habitraca.Domain.Entities
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Habitraca.Domain.Entities
 {
-    internal class User
+    public class User : IdentityUser
     {
+       
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; } 
+       
+        public string? Password { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; }
+        public string? PasswordResetToken { get; set; }
+        public DateTime? ResetTokenExpires { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime DateModified { get; set; }
     }
 }
