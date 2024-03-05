ad30cb828d4893fa953122db675e450ea1a8cbd3
CHUKWUDI INNOCENT <125125776+Innochu@users.noreply.github.com>
2024-03-05 06:37:26 +0100

    seeder identity and User entity

---
+++ Habitraca/Habitraca.Common/Seeder.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Habitraca.Common
+{
+    public class Seeder
+    {
+        public static void SeedRoles(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            if (!roleManager.RoleExistsAsync("Admin").Result)
+			{
+				var role = new IdentityRole("Admin");
+				roleManager.CreateAsync(role).Wait();
+			}
+
+            if (!roleManager.RoleExistsAsync("User").Result)
+            {
+                var role = new IdentityRole("User");
+                roleManager.CreateAsync(role).Wait();
+            }
+        }
+    }
+}
