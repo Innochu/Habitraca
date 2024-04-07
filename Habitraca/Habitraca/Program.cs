3eb9d45245e27174cf8c42e0de8c32117ff2cf07
CHUKWUDI INNOCENT <125125776+Innochu@users.noreply.github.com>
2024-04-08 00:40:03 +0100

    IsProduction/program.cs

--- Habitraca/Habitraca/Program.cs
+++ Habitraca/Habitraca/Program.cs
@@ -48,7 +48,7 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
