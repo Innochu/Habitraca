961674a53739490a4a87785beae0a0bb206e8803
CHUKWUDI INNOCENT <125125776+Innochu@users.noreply.github.com>
2024-04-14 22:24:52 +0100

    swaggergen/config

--- Habitraca/Habitraca/Program.cs
+++ Habitraca/Habitraca/Program.cs
@@ -50,8 +50,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Habitraca-v1");
+        c.RoutePrefix = "swagger";
+    });
 }
 
 using (var scope = app.Services.CreateScope())
